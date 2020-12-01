85ffbe0c3c81a29073b24301a0e2c7cfd19174f0
S3cBar0n <59548030+S3cBar0n@users.noreply.github.com>
2020-12-01 10:38:36 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+TOKEN = "TOKEN"
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
